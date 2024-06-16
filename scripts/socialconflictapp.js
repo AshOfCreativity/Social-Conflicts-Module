9dd52c33ba83f90e075fb8855e5b8504a0627990
AshOfCreativity <96663505+AshOfCreativity@users.noreply.github.com>
2024-06-16 18:58:50 -0500

    Update socialconflictapp.js

--- scripts/socialconflictapp.js
+++ scripts/socialconflictapp.js
@@ -1,11 +1,40 @@
-
 class SocialConflictApp extends FormApplication {
-  // Override necessary methods and create the form here
+  constructor(object = {}, options = {}) {
+    super(object, options);
+  }
+
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      title: "Social Conflict Application",
+      template: "modules/social-conflict-module/templates/factions.html",
+      width: 600,
+      height: 400,
+      closeOnSubmit: true,
+    });
+  }
+
+  async getData() {
+    // Assuming 'module' is an instance of SocialConflictModule
+    return {
+      factions: module.factions
+    };
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+    // Add your event listeners here
+  }
+
+  _updateObject(event, formData) {
+    // Handle form submission here
+  }
 }
 
 Hooks.once('init', async function() {
   console.log('my-social-conflict-module | Initializing Social Conflict App');
   game.socialConflictApp = new SocialConflictApp();
-  game.socialConflictApp.render(true);
 });
 
+Hooks.once('ready', async function() {
+  game.socialConflictApp.render(true);
+});
