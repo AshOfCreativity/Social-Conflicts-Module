f0b49b39c97de160c40b31fa5df971162696366d
AshOfCreativity <96663505+AshOfCreativity@users.noreply.github.com>
2024-06-16 17:28:28 -0500

    Update socialconflictapp.js

--- scripts/socialconflictapp.js
+++ scripts/socialconflictapp.js
@@ -1,8 +1,11 @@
+
 class SocialConflictApp extends FormApplication {
   // Override necessary methods and create the form here
 }
 
 Hooks.once('init', async function() {
+  console.log('my-social-conflict-module | Initializing Social Conflict App');
   game.socialConflictApp = new SocialConflictApp();
   game.socialConflictApp.render(true);
 });
+
