e39644a8461c6516b62aea201ca8142afd589a9b
AshOfCreativity <96663505+AshOfCreativity@users.noreply.github.com>
2024-06-16 15:22:51 -0500

    Create socialconflictmodule.js

---
+++ socialconflictmodule.js
@@ -0,0 +1,52 @@
+class SocialConflictModule {
+  constructor() {
+    this.factions = [];
+  }
+
+  addFaction(name, priority) {
+    const faction = { name, priority, strongholds: [] };
+    this.factions.push(faction);
+    this.shelveFactions();
+  }
+
+  addStronghold(factionName, strongholdName) {
+    const faction = this.factions.find(f => f.name === factionName);
+    if (faction) {
+      faction.strongholds.push({ name: strongholdName, encounters: [] });
+    }
+  }
+
+  generateEncounter(factionName, strongholdName, scale) {
+    const faction = this.factions.find(f => f.name === factionName);
+    const stronghold = faction.strongholds.find(s => s.name === strongholdName);
+    if (stronghold) {
+      const encounter = {
+        description: `This is a generated ${scale} encounter.`,
+        scale
+      };
+      stronghold.encounters.push(encounter);
+    }
+  }
+
+  connectEncounters(factionName1, strongholdName1, factionName2, strongholdName2) {
+    const faction1 = this.factions.find(f => f.name === factionName1);
+    const faction2 = this.factions.find(f => f.name === factionName2);
+    const stronghold1 = faction1.strongholds.find(s => s.name === strongholdName1);
+    const stronghold2 = faction2.strongholds.find(s => s.name === strongholdName2);
+    if (stronghold1 && stronghold2) {
+      stronghold1.encounters.push(...stronghold2.encounters);
+    }
+  }
+
+  shelveFactions() {
+    this.factions.sort((a, b) => b.priority - a.priority);
+    // Implement UI update logic here
+  }
+}
+
+const module = new SocialConflictModule();
+module.addFaction('Faction 1', 2);
+module.addFaction('Faction 2', 1);
+module.addStronghold('Faction 1', 'Stronghold 1');
+module.generateEncounter('Faction 1', 'Stronghold 1', 'player');
+module.shelveFactions();
